aa92e09c522d24c89b43a6681ddf8f44ab2ac105
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-11 00:16:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |k,v|
-    return "Happy Birthday #{k}! You are now #{v} years old!"
+    puts "Happy Birthday #{k}! You are now #{v} years old!"
   end
   # add your code snippet here!
 end
